93929d0568a56021a6d045393f535fe8e8fbaa28
Lien Font <fontlien.@gmail.com>
2026-03-11 18:28:25 -0400

    edited the relationships to be non-gendered and added relationship types such as lovers, exes, ex-spouses

--- src/store/format-data.ts
+++ src/store/format-data.ts
@@ -1,60 +1,66 @@
-import { Data, Datum } from "../types/data"
+import { Data, Datum } from '../types/data';
 
 export interface LegacyDatum extends Omit<Datum, 'rels'> {
-  rels: {
-    father?: string;
-    mother?: string;
-    spouses?: string[];
-    children?: string[];
+	rels: {
+		parent1?: string;
+		parent2?: string;
+		spouses?: string[];
+		children?: string[];
 
-    parents?: string[];
-  };
+		parents?: string[];
+	};
 }
 
 export function formatData(data: any) {
-  data.forEach((d: LegacyDatum) => {
-    if (!d.rels.parents) d.rels.parents = []
-    if (!d.rels.spouses) d.rels.spouses = []
-    if (!d.rels.children) d.rels.children = []
+	data.forEach((d: LegacyDatum) => {
+		if (!d.rels.parents) d.rels.parents = [];
+		if (!d.rels.spouses) d.rels.spouses = [];
+		if (!d.rels.children) d.rels.children = [];
 
-    convertFatherMotherToParents(d)
-  })
-  return data as Data
+		convertFatherMotherToParents(d);
+	});
+	return data as Data;
 
-  function convertFatherMotherToParents(d:LegacyDatum) {
-    if (!d.rels.parents) d.rels.parents = []
-    if (d.rels.father) d.rels.parents.push(d.rels.father)
-    if (d.rels.mother) d.rels.parents.push(d.rels.mother)
-    delete d.rels.father
-    delete d.rels.mother
-  }
+	function convertFatherMotherToParents(d: LegacyDatum) {
+		if (!d.rels.parents) d.rels.parents = [];
+		if (d.rels.parent1) d.rels.parents.push(d.rels.parent1);
+		if (d.rels.parent2) d.rels.parents.push(d.rels.parent2);
+		delete d.rels.parent1;
+		delete d.rels.parent2;
+	}
 }
 
-export function formatDataForExport(data: LegacyDatum[], legacy_format: boolean = false) {
-  data.forEach(d => {
-    if (legacy_format) {
-      let father: Datum['id'] | undefined;
-      let mother: Datum['id'] | undefined;
-      d.rels.parents?.forEach(p => {
-        const parent = data.find(d => d.id === p)
-        if (!parent) throw new Error('Parent not found')
-        if (parent.data.gender === "M") {
-          if (!father) father = parent.id
-          else mother = parent.id   // for same sex parents, we set some parent to father and some to mother
-        }
-        if (parent.data.gender === "F") {
-          if (!mother) mother = parent.id
-          else father = parent.id   // for same sex parents, we set some parent to father and some to mother
-        }
-      })
-      if (father) d.rels.father = father
-      if (mother) d.rels.mother = mother
-  
-      delete d.rels.parents
-    }
-    if (d.rels.parents && d.rels.parents.length === 0) delete d.rels.parents
-    if (d.rels.spouses && d.rels.spouses.length === 0) delete d.rels.spouses
-    if (d.rels.children && d.rels.children.length === 0) delete d.rels.children
-  })
-  return data
+export function formatDataForExport(
+	data: LegacyDatum[],
+	legacy_format: boolean = false,
+) {
+	data.forEach((d) => {
+		if (legacy_format) {
+			let parent1: Datum['id'] | undefined;
+			let parent2: Datum['id'] | undefined;
+			d.rels.parents?.forEach((p) => {
+				const parent = data.find((d) => d.id === p);
+				if (!parent) throw new Error('Parent not found');
+				if (parent.data.gender === 'M') {
+					if (!parent1) parent1 = parent.id;
+					else parent2 = parent.id; // for same sex parents, we set some parent to father and some to mother
+				}
+				if (parent.data.gender === 'F') {
+					if (!parent2) parent2 = parent.id;
+					else parent1 = parent.id; // for same sex parents, we set some parent to father and some to mother
+				}
+			});
+			if (parent1) d.rels.parent1 = parent1;
+			if (parent2) d.rels.parent2 = parent2;
+
+			delete d.rels.parents;
+		}
+		if (d.rels.parents && d.rels.parents.length === 0)
+			delete d.rels.parents;
+		if (d.rels.spouses && d.rels.spouses.length === 0)
+			delete d.rels.spouses;
+		if (d.rels.children && d.rels.children.length === 0)
+			delete d.rels.children;
+	});
+	return data;
 }
--- src/types/data.ts
+++ src/types/data.ts
@@ -1,15 +1,18 @@
 export interface Datum {
-  id: string;
-  data: {
-    gender: 'M' | 'F';
-    [key: string]: any;
-  };
-  rels: {
-    parents: string[];
-    spouses: string[];
-    children: string[];
-  };
-  [key: string]: any;
+	id: string;
+	data: {
+		gender: 'M' | 'F' | 'U';
+		[key: string]: any;
+	};
+	rels: {
+		parents: string[];
+		spouses: string[];
+		exSpouses: string[];
+		lovers: string[];
+		exLovers: string[];
+		children: string[];
+	};
+	[key: string]: any;
 }
 
 export type Data = Datum[];
